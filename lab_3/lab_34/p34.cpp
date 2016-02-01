7a9fda4e7f90720cfd0e1ae551219dc9385f84c0
a3zzat <viva.amego@gmail.com>
2016-02-01 23:46:36 +0100

    working version
    please test it for all files
    
    use these commands :
    
    make
    clang -c -emit-llvm test1.c -o test1.bc (for all tests)
    opt -load ./p34.so -fix-pass test4.bc -o test4_fix.bc 2> test4.fix (for all tests)
    llvm-dis< test4_fix.bc >test4_fix_dis.dis
    llvm-dis< test4.bc >test4_dis.dis
    meld
    (then compare "test4_fix_dis.dis", "test4_dis.dis")

--- lab_3/lab_34/p34.cpp
+++ lab_3/lab_34/p34.cpp
@@ -6,24 +6,16 @@
 	#include <utility>
 
 	// LLVM
-	/*#include <llvm/Pass.h>
+	#include <llvm/Pass.h>
 	#include <llvm/IR/LLVMContext.h>
 	#include <llvm/IR/Function.h>
+	#include <llvm/IR/Instruction.h>
 	#include <llvm/IR/Instructions.h>
 	#include "llvm/Analysis/CFG.h"
 	#include <llvm/Support/raw_ostream.h>
 	#include <llvm/IR/InstIterator.h>
-	#include <llvm/IR/Constants.h>*/
-         #include <llvm/Pass.h>
-	#include <llvm/IR/LLVMContext.h>
-	#include <llvm/IR/Function.h>
-        #include <llvm/IR/Instruction.h>
-	#include <llvm/IR/Instructions.h>
-	#include "llvm/Support/CFG.h"
-	#include <llvm/Support/raw_ostream.h>
-	#include <llvm/Support/InstIterator.h>
 	#include <llvm/IR/Constants.h>
-        
+    #include <llvm/IR/DerivedTypes.h>
 
 	using namespace llvm;
 
@@ -57,7 +49,7 @@
       						newvar.isNotified = false;
 									varInitMap[ai->getName().str()] = newvar;
       						if(verbose)
-      							errs() << ai->getName()<< " allocated\n";//TODO
+      							errs() << ai->getName()<< " alocated\n";//TODO
       					}
 					}
 					if (CallInst *ci = dyn_cast<CallInst>(&*ii)) {
@@ -117,80 +109,125 @@
 		struct VarInfo {
 		   bool isInitialized;
 		   bool isNotified;
-                   PointerType *Ais;
+			 AllocaInst *Ais;
+           std::string InitAns = "No";
+           std::string varType;
 		};
-	
+		static const bool verbose = true;
+
 		virtual bool runOnFunction(Function &F){
 			std::map <std::string, VarInfo> varInitMap;
 			for(BasicBlock &b : F.getBasicBlockList()){ //for each block found in function
-					for (BasicBlock::iterator ii = b.begin(), ie = b.end(); ii != ie; ++ii) {  //Iterate over the instructions of each block
-					          if (AllocaInst *ai = dyn_cast<AllocaInst>(&*ii)) { //If instruction is an allocation
-						         if(ai->hasName()) //And it has a name
-						            {
-							      VarInfo newvar;
-							      newvar.isInitialized = false;
-							      newvar.isNotified = false;
-                                          		      newvar.Ais = ai->getType();
- 							      varInitMap[ai->getName().str()] = newvar;
-							
-						              }
-			                                }
-			                         if (CallInst *ci = dyn_cast<CallInst>(&*ii)) {
-				                    //Do something with call intrustion
-					            errs()<< ci->getCalledFunction()->getName()<<" function\n";
+                for (BasicBlock::iterator ii = b.begin(), ie = b.end(); ii != ie; ++ii) {  //Iterate over the instructions of each block
+					if (AllocaInst *ai = dyn_cast<AllocaInst>(&*ii))
+					{ //If instruction is an allocation
+						if(ai->hasName()) //And it has a name
+						{
+							VarInfo newvar;
+							newvar.isInitialized = false;
+							newvar.isNotified = false;
+							newvar.Ais = ai;
+ 							std::string VarName = ai->getName().str();
+ 							varInitMap[VarName] = newvar;
+                            if(varInitMap[VarName].Ais->getAllocatedType()->isDoubleTy())
+                            {
+                                varInitMap[VarName].varType = "Double";
+                            }
+                            else if(varInitMap[VarName].Ais->getAllocatedType()->isFloatTy())
+                            {
+                                varInitMap[VarName].varType = "Float";
+                            }
+                            else if(varInitMap[VarName].Ais->getAllocatedType()->isIntegerTy())
+                            {
+                                varInitMap[VarName].varType = "Integer";
+                            }
+							if(verbose)
+								errs() << "alocated Variable "<<VarName<< "\n";
+						}
+                    }
+                if (CallInst *ci = dyn_cast<CallInst>(&*ii)) {
+                    //Do something with call intrustion
+                       if(verbose) errs()<<"call Function : " <<ci->getCalledFunction()->getName()<<" \n";
+                }
+                if (StoreInst *si = dyn_cast<StoreInst>(&*ii)) {
+                    if (si->getOperand(1)->hasName())
+                    {
+                        varInitMap[si->getOperand(1)->getName().str()].isInitialized = true;
+                        varInitMap[si->getOperand(1)->getName().str()].InitAns = "YES";
+                        if(verbose)
+                            errs() <<"variable "<< si->getOperand(1)->getName()<< " : is initialized\n";
+                    }
+                }
+                if (LoadInst *li = dyn_cast<LoadInst>(&*ii))
+                {
+                    if (li->getOperand(0)->hasName())
+                    {
+                        std::string varName = li->getOperand(0)->getName().str();
 
-			                            }
-			                        if (StoreInst *si = dyn_cast<StoreInst>(&*ii)) {
-				                     if (si->getOperand(1)->hasName())
-				                         {
-							varInitMap[si->getOperand(1)->getName().str()].isInitialized = true;
-				                         }
-			                             }
+                        if(verbose)
+                        {
+                            errs() << "Load Instruction: \n Is Variable " << varName << " initialized?  " << varInitMap[varName].InitAns <<"\n Variable type = "<<varInitMap[varName].varType <<"\n";
+                        }else
+                        {
+                            errs() <<varName<<"\n";
+                        }
+                        if( varInitMap[varName].isInitialized == false && varInitMap[varName].isNotified == false)
+                        {
+                            varInitMap[varName].isNotified = true;
+                            if (varInitMap[varName].varType == "Double")
+                            {
+                                double d_init = 30.0;
+                                if(verbose) errs()<< "intializing var ..    value =" << d_init<<"\n";
+                                Value *num = ConstantFP::get(Type::getDoubleTy(b.getContext()), d_init);
+                                StoreInst *my_store = new StoreInst(num,varInitMap[varName].Ais);
+                                my_store->insertBefore(li);
 
-			                       if (LoadInst *li = dyn_cast<LoadInst>(&*ii)) {
-						 LoadInst* insertbefore = nullptr;
-				
-				                     std::string varName = li->getOperand(0)->getName().str();
-				                      if (li->getOperand(0)->hasName() && varInitMap[varName].isInitialized == false && varInitMap[varName].isNotified == false)
-				                          {
+                            }
+                            else if (varInitMap[varName].varType == "Float")
+                            {
+                                float f_init = 20.0;
+                                if(verbose) errs()<< "intializing var ..    value =" << f_init<<"\n";
+                                Value *num = ConstantFP::get(Type::getFloatTy(b.getContext()), f_init);
+                                StoreInst *my_store = new StoreInst(num,varInitMap[varName].Ais);
+                                my_store->insertBefore(li);
+                            }
+                            else if (varInitMap[varName].varType == "Integer")
+                            {
+                                int i_init = 10 ;
+                                if(verbose) errs()<< "intializing var ..    value =" << i_init<<"\n";
+                                Value *num = ConstantInt::get(Type::getInt32Ty(b.getContext()), i_init);
+                                StoreInst *my_store = new StoreInst(num,varInitMap[varName].Ais);
+                                my_store->insertBefore(li);
+                            }
+                            else
+                            {
+                                errs() << " unknown type";
+                            }
 
-				                             //IntegerType *int_type = Type::getInt64Ty(context);
-                                                           IntegerType *int_type = IntegerType::get(li->getContext(), 64);
-				                           if(varInitMap[varName].Ais->getElementType()->isIntegerTy())
-					                      {
-					                            Value *num = ConstantInt::get(int_type, 10, true);
-                                                                    Value *alloc = new AllocaInst(int_type, "int_addr", &b);
-                                                                    //StoreInst *ptr = new StoreInst(num,alloc,false,&b);
-									StoreInst *ptr = new StoreInst(num,alloc,false,insertbefore);
-                   							li->getParent()->getInstList().insert(li, ptr);
-					                      }
-				                            else if(varInitMap[varName].Ais->getElementType()->isDoubleTy())
-					                          {
-					                            Value *num = ConstantInt::get(int_type, 30.0, true);
-                                                                    Value *alloc = new AllocaInst(int_type, "double_addr", &b);
-                                                                     //StoreInst *ptr = new StoreInst(num,alloc,false,&b);
-								    StoreInst *ptr = new StoreInst(num,alloc,false,insertbefore);
-									li->getParent()->getInstList().insert(li, ptr);
-					                          }
-				                            else if(varInitMap[varName].Ais->getElementType()->isFloatTy())
-				                                   {
-					                              Value *num = ConstantInt::get(int_type, 20.0, true);
-                                                                      Value *alloc = new AllocaInst(int_type, "float_addr", &b);
-                                                                       // StoreInst *ptr = new StoreInst(num,alloc,false,&b);
-									StoreInst *ptr = new StoreInst(num,alloc,false,insertbefore);
-                                                       			li->getParent()->getInstList().insert(li, ptr);
-					                           }
-                                                            }
-				                          varInitMap[varName].isNotified = true;
-			                          }
-			                }
+                        }
+                    }
+                }
+            }
 
-			               }
+        }
 
+        //Print all maps values
+        if(verbose)
+        {
+		errs() << "new Block:\n";
+		std::map<std::string, VarInfo>::iterator variablesMapIterator = varInitMap.begin();
+		while (variablesMapIterator!=varInitMap.end())
+		{
+			errs()<<"Iterator: " << variablesMapIterator->first << "\n";
+			errs()<<"    isInitialized: " << variablesMapIterator->second.isInitialized << "\n";
+			errs()<<"    isNotified: " << variablesMapIterator->second.isNotified << "\n";
+			variablesMapIterator++;
+		}
+	}
 	            errs() << "fix-pass\n";
 		    return true;
-		}
-	};
+    }
+};
 	} // namespace
 
 
